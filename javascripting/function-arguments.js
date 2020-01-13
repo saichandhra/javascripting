a81186213272181379f69b9c579eb7bc3c7447db
saichandhra <59429807+saichandhra@users.noreply.github.com>
2020-01-13 18:30:21 +0530

    Add files via upload

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,7 @@
+function math(a,b,c)
+{
+
+return (c*b)+a
+
+}
+console.log(math(53,61,67))
---
+++ javascripting/functions.js
@@ -0,0 +1,6 @@
+function eats(bananas){
+return bananas+=' tasted really good.' 
+
+}
+console.log(eats('bananas'))
+
